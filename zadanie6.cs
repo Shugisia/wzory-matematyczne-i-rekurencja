5c1399de844d3e3d4f7d7e88cc451861178a044d
Wanessa Witkowska <witkowskav@gmail.com>
2023-06-13 16:07:24 +0200

    Create zadanie6.cs

---
+++ zadanie6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj liczbę x: ");
+        int x = int.Parse(Console.ReadLine());
+
+        int n = 0;
+        int factorial = 1;
+
+        while (factorial <= x)
+        {
+            n++;
+            factorial *= n;
+        }
+
+        if (factorial == x)
+        {
+            Console.WriteLine("Liczba {0} jest silnią liczby {1}.", x, n);
+        }
+        else
+        {
+            Console.WriteLine("Liczba {0} nie jest silnią żadnej liczby.", x);
+        }
+    }
+}
