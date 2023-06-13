3a1372ab48b98005cca1ae0ec0837c85fa1241a7
Wanessa Witkowska <witkowskav@gmail.com>
2023-06-13 16:07:09 +0200

    Create zadanie9.cs

---
+++ zadanie9.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Podaj liczbę do odwrócenia: ");
+        int liczba = int.Parse(Console.ReadLine());
+        int odwliczba = OdwLiczba(liczba);
+
+        Console.WriteLine(odwliczba);
+    }
+
+    public static int OdwLiczba(int liczba)
+    {
+        int odwliczba = 0;
+
+        while (liczba > 0)
+        {
+            int remainder = liczba % 10;
+            odwliczba = (odwliczba * 10) + remainder;
+            liczba /= 10;
+        }
+
+        return odwliczba;
+    }
+}
