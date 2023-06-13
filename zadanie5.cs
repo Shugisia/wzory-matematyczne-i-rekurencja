f39b009bc8063222ff9851412639e94646734563
Wanessa Witkowska <witkowskav@gmail.com>
2023-06-13 16:07:20 +0200

    Create zadanie5.cs

---
+++ zadanie5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        int numerPrzyb, i;
+        double wartoscPrzyb;
+
+        for (i = 0; i <= numerPrzyb; i++)
+        {
+            wartoscPrzyb = wartoscPrzyb + sqrt(wartoscPrzyb) / silnia;
+        }
+        Console.WriteLine(wartoscPrzyb);
+
+        static int silnia(int numer)
+        {
+            int psilnia = 1;
+            for (int i = 2; i <= numer; i++)
+            {
+                psilnia *= i;
+            }
+            return psilnia;
+        }
+    }
+}
