ab4250c85042c7732d3a7521bce3ec435377a8dd
Wanessa Witkowska <witkowskav@gmail.com>
2023-06-13 16:07:12 +0200

    Create zadanie1.cs

---
+++ zadanie1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int podstawa, wykladnik;
+
+        Console.WriteLine("Podaj podstawę mnożenia:");
+        podstawa = (int)inputValue();
+        Console.WriteLine("Podaj wykładnik potęgowania");
+        wykladnik = (int)inputValue();
+
+        int wynik = 1;
+
+        for (int i = 1; i <= wykladnik; i++)
+        {
+            wynik *= podstawa;
+        }
+
+        Console.WriteLine("Wynik potęgowania: " + wynik);
+    }
+
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
