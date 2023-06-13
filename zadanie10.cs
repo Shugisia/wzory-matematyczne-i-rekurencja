8daf6a6fea4ca6323a2e88edc57f1293443f8d34
Wanessa Witkowska <witkowskav@gmail.com>
2023-06-13 16:07:13 +0200

    Create zadanie10.cs

---
+++ zadanie10.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj liczbę naturalną posiadającą co najmniej 3 cyfry: ");
+        int liczba = int.Parse(Console.ReadLine());
+
+        if (liczba < 100)
+        {
+            Console.WriteLine("Podana liczba musi mieć przynajmniej 3 cyfry.");
+        }
+        else
+        {
+            Console.WriteLine("Poszczególne cyfry liczby:");
+
+            string liczbaTekst = liczba.ToString();
+            foreach (char cyfra in liczbaTekst)
+            {
+                Console.WriteLine(cyfra);
+            }
+        }
+    }
+}
+
+
+
+
+
+
