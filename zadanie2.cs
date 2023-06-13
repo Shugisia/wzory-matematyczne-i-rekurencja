b8f211e465376506f18e01e52f280dfc77b097a7
Wanessa Witkowska <witkowskav@gmail.com>
2023-06-13 16:07:15 +0200

    Create zadanie2.cs

---
+++ zadanie2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Fibonacci
+{
+    public static void Main(string[] args)
+    {
+        int n;
+        Console.WriteLine("Podaj długość ciągu Fibonacciego: ");
+        n = (int)inputValue();
+        CiagFibonacci(n);
+    }
+
+    public static void CiagFibonacci(int n)
+    {
+        int pierwszy = 0; 
+        int drugi = 1; 
+
+        Console.WriteLine("Ciąg Fibonacciego:");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(pierwszy);
+            int nastepny = pierwszy + drugi;
+            pierwszy = drugi;
+            drugi = nastepny;
+        }
+    }
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
