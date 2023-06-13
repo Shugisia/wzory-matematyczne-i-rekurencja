f510e253fd219e6a72a22ec176405ccd7e357659
Wanessa Witkowska <witkowskav@gmail.com>
2023-06-13 16:07:16 +0200

    Create zadanie3.cs

---
+++ zadanie3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n, k;
+
+        do
+        {
+            Console.Write("Podaj liczbę n (większą lub równą 5): ");
+        } while (!int.TryParse(Console.ReadLine(), out n) || n < 5);
+
+        do
+        {
+            Console.Write("Podaj liczbę k (większą lub równą 5): ");
+        } while (!int.TryParse(Console.ReadLine(), out k) || k < 5);
+
+        
+        int nSilnia = Silnia(n); //n!       
+        int kSilnia = Silnia(k);  //k!
+       
+        int m = (nSilnia - kSilnia) / kSilnia; //m = (n! - k!) / k!
+
+        Console.WriteLine($"Wartość m=(n!-k!)/k! wynosi: {m}");
+        Console.ReadLine();
+    }
+
+    static int Silnia(int numer)
+    {
+        int psilnia = 1;
+        for (int i = 2; i <= numer; i++)
+        {
+            psilnia *= i;
+        }
+        return psilnia;
+    }
+}
